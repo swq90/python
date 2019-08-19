097d345c6bd9437f98995736bb391022a3f47d16
Jane <shaowenqin620@163.com>
2019-08-19 15:38:18 +0800

    leetcode 122

---
+++ leetcode/122best_time_to_buy_and_sell_stock_2.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) < 2:
+            return 0
+        profit, min_price = 0, prices[0]
+        for price in prices[1:]:
+            if price < min_price:
+                min_price = price
+            else:
+                profit += price - min_price
+                min_price = price
+        return profit
+
+
+
+    def maxProfit3(self, prices):
+        if len(prices) < 2:
+            return 0
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        return profit
+
+
+
+
+
+
+
+
+#允许多次交易
