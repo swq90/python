2ca368df99306adfce1f553dfa1cc380c2f025d2
Jane <shaowenqin620@163.com>
2019-09-17 22:51:37 +0800

    tensorflow

--- tensorflow/chapter12/12.1.py
+++ tensorflow/chapter12/12.1.py
@@ -1,30 +1,30 @@
 import tensorflow as tf
-# a= tf.compat.v1.constant([1.0, 2.0,3.0], shape=[3] , name= 'a')
-# b= tf.compat.v1.constant([1.0, 2.0,3.0], shape=[3] , name= 'b')
-# c = a + b
-# # 边过 log_device_placement 参数来输出运行每一个运算的设备。
-# sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
-# print(sess . run(c))
-# # 在没有 GPU的机器上运行以上代码可以得到类似以下的输出 ：
-# # Device mapping: no known devices .
-# # add:(Add): /job:localhost/replica:O/task:O/cpu:O
-# # b : (Const): /job: localhost/replica: 0/task: 0/cpu: 0
-# # a : (Const): /job: localhost/replica: 0/task: 0/cpu : 0 [ 2 . 4 . 6 . ] ”
+a= tf.compat.v1.constant([1.0, 2.0,3.0], shape=[3] , name= 'a')
+b= tf.compat.v1.constant([1.0, 2.0,3.0], shape=[3] , name= 'b')
+c = a + b
+# 边过 log_device_placement 参数来输出运行每一个运算的设备。
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
+print(sess . run(c))
+# 在没有 GPU的机器上运行以上代码可以得到类似以下的输出 ：
+# Device mapping: no known devices .
+# add:(Add): /job:localhost/replica:O/task:O/cpu:O
+# b : (Const): /job: localhost/replica: 0/task: 0/cpu: 0
+# a : (Const): /job: localhost/replica: 0/task: 0/cpu : 0 [ 2 . 4 . 6 . ] ”
 
 
-#
-# with tf.device('/cpu:0'):
-#     a= tf.compat.v1.constant([1.0, 2.0,3.0], shape=[3] , name= 'a')
-#     b= tf.compat.v1.constant([1.0, 2.0,3.0], shape=[3] , name= 'b')
-# with tf.device('/gpu:1'):
-#     c = a + b
-# # 边过 log_device_placement 参数来输出运行每一个运算的设备。
-# sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
-# print(sess . run(c))
+
+with tf.device('/cpu:0'):
+    a= tf.compat.v1.constant([1.0, 2.0,3.0], shape=[3] , name= 'a')
+    b= tf.compat.v1.constant([1.0, 2.0,3.0], shape=[3] , name= 'b')
+with tf.device('/gpu:1'):
+    c = a + b
+# 通过 log_device_placement 参数来输出运行每一个运算的设备。
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
+print(sess . run(c))
 
 
 a_cpu = tf.Variable(0, name='a_cpu')
-with tf.device ('/gpu:0'):
+with tf.device('/gpu:0'):
     a_gpu = tf.Variable(0, name='a_gpu')
 # 通过 allow_soft_placement参数自动将无法放在GPU上的操作放回CPU
 sess = tf.compat.v1.Session(config=tf.ConfigProto(allow_soft_placement=True,log_device_placement=True))
--- tensorflow/chapter12/note.md
+++ tensorflow/chapter12/note.md
@@ -40,17 +40,62 @@ sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement
 print(sess . run(c))
 ```
 
-在 TensorFlow 中，不是所有的操作都可以被放在 GPU 上，如 果强行将无法放在 GPU 上的操作指定到 GPU 上，那么程序将会报错。
-在 TensorFlow 的 kernel中定义了哪些操作可以跑在 GPU 上。
-TensorFlow 在生成会话时可以指定 allow_soft _placement 参数。当 allow_soft_placement 参 数设置为 True 时，如果运算无法由 GPU 执行，那么 TensorFlow 会自动将它放到 CPU 上执 行
+在 TensorFlow 中，不是所有的操作都可以被放在GPU上，如果强行将无法放在GPU上的操作指定到GPU上，那么程序将会报错。
+在 TensorFlow 的kernel中定义了哪些操作可以跑在GPU上。
+TensorFlow 在生成会话时可以指定allow_soft_placement参数。当allow_soft_placement参数设置为True时，如果运算无法由GPU执行，那么TensorFlow会自动将它放到CPU上执行
 ```python
+import tensorflow as tf
 
-
+a_cpu = tf.Variable(0, name='a_cpu')
+with tf.device('/gpu:0'):
+    a_gpu = tf.Variable(0, name='a_gpu')
+# 通过 allow_soft_placement参数自动将无法放在GPU上的操作放回CPU
+sess = tf.compat.v1.Session(config=tf.ConfigProto(allow_soft_placement=True,log_device_placement=True))
+sess.run(tf.initialize_all_variables())
 
 ```
+一般来说不会把所有的操作全部放在GPU上。一个比较好的实践是将计算密集型的运算放在GPU上，而把其他操作放到CPU上。
+GPU是机器中相对独立的资源，将计算放入或者转出GPU都需要额外的时间。而且GPU需要将计算时用到的数据从内存复制到GPU设备上，这也需要额外的时间。TensorFlow可以自动完成这些操作而不需要用户特别处理，但为了提高程序运行的速度，用户也需要尽量将相关的运算放在同一个设备上。
+
+TensorFlow默认会占用设备上的所有GPU以及每个GPU的所有显存。如果在一个TensorFlow程序中只需要使用部分GPU，
+可以通过设置CUDA_VISIBLE_DEVICES环境变量来控制
+```
+# 只使用第二块GPU(GPU编号从0开始）。在demo_code.py中，机器上的第二块GPU的名称变成／gpu:0，不过在运行时所街／gpu:0的运算将被放在第二块GPU上。
+CUDA_VISIBLE_DEVICES=1 python demo_code.py
+# 只使用第一块和第二块GPU
+CUDA_VISIBLE_DEVICES=0,1 python demo_code.py
+```
+TensorFlow也支持在程序中设置环境变量
+```python
+import os
+
+# 只用第三块GPU
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
+```
+虽然TensorFlow默认会一次性占用一个GPU的所有显存，但是TensorFlow也支持动态分配GPU的显存，使得一块GPU上可以同时运行多个任务  
+
+```python
+import tensorflow as tf
 
 
+config = tf.ConfigProto()
+#让tensorflow按需分配显存
+
+config.gpu_options.allow_growth = True
+
+# 或者直接按固定的比例分配，以下代码会占用所有可使用GPU的40%现存
+# config.gpu_options.per_process_gpu_memory_fraction = 0.4
+session = tf.compat.v1.Session(config=config, )
+
+```
 ##12.2 深度学习训练并行模式
+
+TensorFlow 可以很容易地利用单个GPU加速深度学习模型的训练过程，但要利用更多的GPU或者机器，需要了解如何并行化地训练深度学习模型
+常用的并行化深度学习模型训练方式有两种，同步模式和异步模式
+根据当前参数的取值和随机获取的一小部分训练数据，不同设备各自运行反向传播的过程并独立地更新参数。
+异步模式就是单机模式复制了多份，每一份使用不同的训练数据进行训练。在异步模式下，不同设备之间是完全独立的。
+
 #####
 ##12.3 多GPU并行
 ###
