9a757fa2beab273ed9e3509f9a82ef566f7e1a5f
Jane <shaowenqin620@163.com>
2019-10-07 20:02:04 +0800

    leetcode

--- leetcode/4median_of_two_sorted_arrays.py
+++ leetcode/4median_of_two_sorted_arrays.py
@@ -5,23 +5,8 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
-        # m, n= len(nums1), len(nums2)
-        # i,j,count = 0,0,0
-        # median = (m+n)/2
-        # while count<median-1:
-        #     if nums1[i]<= nums2[j]:
-        #         i += 1
-        #     else:
-        #         j += 1
-        #     count += 1
-        # print(i,j,nums1[i],nums2[j],count)
-        # if median - count:
-        #     return min(nums1[i],nums2[j])
-        # return
-        d = sorted(nums1+nums2)
-        if len(d)/2:
-            return d[len(d)//2]
-        return (d[len(d)//2]+d[len(d)//2-1])/2
+        m, n = len(nums1),len(nums2)
+
 
 
 o = Solution()
