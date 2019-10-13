8779a9a1fc29995872f167d92ecf78e41ee2f118
Jane <shaowenqin620@163.com>
2019-10-13 20:40:23 +0800

    tensorflow

--- tensorflow/chapter4/4.2.py
+++ tensorflow/chapter4/4.2.py
@@ -1,11 +1,14 @@
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 import numpy.random
-
+tf.disable_v2_behavior()
 batch_size = 8
 # 两个输入节点
 # 回归问题只有一个输出节点
-x = tf.compat.v1.placeholder(tf.float32, shape=(None, 2), name="x-input")
-y_ = tf.compat.v1.placeholder(tf.float32, shape=(None, 1), name="y-input")
+# tf.compat.v1.disable_eager_execution()
+
+
+x = tf.placeholder(tf.float32, shape=(None, 2), name="x-input")
+y_ = tf.placeholder(tf.float32, shape=(None, 1), name="y-input")
 
 # 向前传播过程，加权和
 w1 = tf.Variable(tf.random_normal([2, 1], stddev=1, seed=1))
@@ -15,8 +18,8 @@ y = tf.matmul(x, w1)
 loss_less = 10
 loss_more = 1
 loss = tf.reduce_sum(tf.where(tf.greater(y, y_), (y-y_)*loss_more, (y_-y)*loss_less))
-# 反向传播的参数,学习率0.001，损失函数
-train_step = tf.compat.v1.train.AdamOptimizer(0.001).minimize(loss)
+# 反向传播,学习率0.001，损失函数
+train_step = tf.train.AdamOptimizer(0.001).minimize(loss)
 
 # 生成模拟数据集
 rdm = numpy.random.RandomState(1)
@@ -30,22 +33,22 @@ X = rdm.rand(dataset_size, 2)
 Y = [[x1 + x2 + rdm . rand()/10.0-0.05] for (x1, x2) in X]
 
 
-saver = tf.train.Saver()
+# saver = tf.train.Saver()
 # 训练神经网络
-with tf.compat.v1.Session() as sess:
-    init_op = tf.compat.v1.global_variables_initializer()
+with tf.Session() as sess:
+    init_op = tf.global_variables_initializer()
     sess.run(init_op)
     STEPS = 5000
     for i in range(STEPS):
         start = (i*batch_size) % dataset_size
         end = min(start+batch_size, dataset_size)
         sess.run(train_step, feed_dict={x: X[start:end], y_: Y[start:end]})
-        print(sess.run(w1))
-        saver_path=saver.save(sess,"save/model.ckpt")
+        # print(sess.run(w1))
+        # saver_path=saver.save(sess,"save/model.ckpt")
 
 
 
 # 载入模型
-saver = tf.train.Saver()
-with tf.Session() as sess:
-    saver.restore(sess, "save/model.ckpt")
+# saver = tf.train.Saver()
+# with tf.Session() as sess:
+#     saver.restore(sess, "save/model.ckpt")
