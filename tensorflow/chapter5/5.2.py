c4eb4f8278c2cbc4098e6370fb0d5ce157740491
Jane <shaowenqin620@163.com>
2019-10-14 23:05:54 +0800

    tensorflow

--- tensorflow/chapter5/5.2.py
+++ tensorflow/chapter5/5.2.py
@@ -6,38 +6,30 @@ INPUT_NODE = 784  # 输入层的节点数。对于MNIST数据集，这个就等�
 OUTPUT_NODE = 10  # 输出层的节点数。这个等于类别的数目。因为在MNIST数据集叶# 市要区分的足O～9这10个数宇，所以这里输出层的节点数为10。
 
 # 配置神经网络的参数。
-# 输出层的节成数。这个等于类别的数目。因为在MNIST数据集叶# 市要区分的足O～9这10个数宇，所以这里输出层的节点数为10。
-# 隐藏层节点数。这里使用只有一个隐藏层的网络结构作为样例。
-# 这个隐藏层有500个节点。
-BATCH_SIZE = 100
-# 一个训练batch中的训练数据个数。数字路小时，训｜陈过程越接近# 随机梯度下降：数字越大时，训练、越接近梯度下降。
-LEARNING_RATE_BASE = 0.8  # 基础的学习率。
+LAYER1_NODE = 500           # 隐藏层节点数这个隐藏层有500个节点。
+BATCH_SIZE = 100            # 一个训练batch中的训练数据个数。数字越小时，训练过程越接近
+                            # 随机梯度下降：数字越大时，训练越接近梯度下降。
+LEARNING_RATE_BASE = 0.8    # 基础的学习率。
 LEARNING_RATE_DECAY = 0.99  # 学习率的衰减率。
-LAYER1_NODE = 500
-REGULARIZATION_RATE = 0.0001
-TRAINING_STEPS = 30000
-MOVING_AVERAGE_DECAY = 0.99
+
+REGULARIZATION_RATE = 0.0001# 描述模型复杂度的正则化项在损失函数中的系数。
+TRAINING_STEPS = 30000      # 训练轮数
+MOVING_AVERAGE_DECAY = 0.99 # 滑动平均衰减率。
 
 
-# 描述模型复杂度的正则化项在损失函数中的系数。
-# 训练轮数。
-# 滑动平均衰减率。
-# 一个辅助函数，给定神经网络的输入和所有参数，计算神经网络的前向传播结果。在这里
-# 定义了一个使用ReLU激活函数的三层全连接神经网络。通过加入隐藏层实现了多层网络结构，
-# 通过ReLU激活函数实现了去线性化。在这个函数中也支持传入用于计算参数平均值的类，
-# 这样方便在测试时使用滑动平均模型。
+# inference辅助函数，给定神经网络的输入和所有参数，计算神经网络的前向传播结果。定义了一个使用ReLU激活函数的三层全连接神经网络。
+# 通过加入隐藏层实现了多层网络结构，通过ReLU激活函数实现了去线性化。也支持传入用于计算参数平均值的类，这样方便在测试时使用滑动平均模型。
 def inference(input_tensor, avg_class, weights1, biases1, weights2, biases2):
     # 当没有提供滑动平均类时，直接使用参数当前的取值。
     if avg_class == None:
-        # 计算隐藏层的前向传播结果，这里使用了ReLUi版活函数。
+        # 计算隐藏层的前向传播结果，这里使用了ReLU激活函数。
         layer1 = tf.nn.relu(tf.matmul(input_tensor, weights1) + biases1)
         # 计算输出层的前向传播结果。因为在计算损失函数时会并计算softmax函数，
         # 所以这里不需要加入激活函数。而且不加入softmax不会影响预测结果。因为预测时
-        # 使用的是不同类别对应节点输出值的相对大小，有没有softmax层对最后分类结果的
-        # 计算没有影响。于是在计算整个神经网络的前向传播时可以不加入最后的softmax层。
+        # 使用的是不同类别对应节点输出值的相对大小，有没有softmax层对最后分类结果的计算没有影响。
+        # 于是在计算整个神经网络的前向传播时可以不加入最后的softmax层。
         return tf.matmul(layer1, weights2) + biases2
 
-        #
     else:
         # 首先使用avgclass.average函数来计算得出变量的滑动平均值，
         # 然后再计算相应的神经网络前向传播结果。
@@ -56,64 +48,57 @@ def train(mnist):
     # 生成输出层的参数。
     weights2 = tf.Variable(tf.truncated_normal([LAYER1_NODE, OUTPUT_NODE], stddev=0.1))
     biases2 = tf.Variable(tf.constant(0.1, shape=[OUTPUT_NODE]))
-    # 计算在当前参数下神经网络前向传播的结果。这里给出的用于计算滑动平均的类为None,
-    # 所以的数不会使用参数的滑动平均值。
+    # 计算在当前参数下神经网络前向传播的结果。这里给出的用于计算滑动平均的类为None所以的数不会使用参数的滑动平均值。
     y = inference(x, None, weights1, biases1, weights2, biases2)
-    # 定义在储训练轮数的变旺。这个变盘不需要计算滑动平均值，所以这里指定这个变量为
-    # 不可训练的变量（trainable=Fasle）。在使用TensorFlow训练神经网络时，
-    # －般会将代友训练轮数的变量指定为不可训练的参数。
+    # 定义存储训练轮数的变量。这个变量不需要计算滑动平均值，所以这里指定这个变量为不可训练的变量
+    # （trainable=Fasle）。在使用TensorFlow训练神经网络时，－般会将训练轮数的变量指定为不可训练的参数。
     global_step = tf.Variable(0, trainable=False)
 
-    # 给定消动平均哀减率和训练轮数的变茧，初始化滑动平均类。在第4章中介绍过给
-    # 定训练轮数的变量可以JJ~i快训练早期变量的更新速度。
+    # 给定滑动平均衰减率和训练轮数的变量，初始化滑动平均类。
+    # 在第4章中介绍过给定训练轮数的变量可以加快训练早期变量的更新速度。
     variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
-    # 在所有代友神经网络参数的变盘上使用滑动平均。其他辅助变量（比如global_step）就
-    # 不需要了。tf.trainablevariables返回的就是图上集合
-    # GraphKeys.TRAINABLEVARIABLES中的元索。这个集合的元索就是所有没有指定
-    # trainable=False的参数。
+    # 在所有代表神经网络参数的变量上使用滑动平均。其他辅助变量（比如global_step）就不需要了
+    # tf.trainablevariables返回的就是图上集合，GraphKeys.TRAINABLEVARIABLES中的元索。
+    # 这个集合的元索就是所有没有指定trainable=False的参数。
     variables_averages_op = variable_averages.apply(tf.trainable_variables())
-    # 计算使用了滑动平均之后的前向传播结果。第4章巾介绍过滑动平均不会改变变fil本身的取值
-    # 而是会维护一个澎子变量来记正式其滑动平均值。所以当面要使用这个滑动平均值时，
-    # 需要明确调用average函数。
+    # 计算使用了滑动平均之后的前向传播结果。第4章介绍过滑动平均不会改变变量本身的取值
+    # 而是会维护一个影子变量来记录滑动平均值。所以当面要使用这个滑动平均值时，需要明确调用average函数。
     average_y = inference(x, variable_averages, weights1, biases1, weights2, biases2)
-    # 计算交叉；胸作为刻画顶训值和真实值之间差距的损失函数。这里使用了TensorFlow中提
-    # 供的sparse_softmax_cross_entropy_with_log工ts函数来计算交叉焰。当分类
-    # 问题只打一个正确答案时，可以使用这个函数来加速交叉娟的计算。MNIST问题的图片中
-    # #n包含了。～9巾的－个数字，所以可以使用这个函数来计算交叉娟损失。这个函数的第一个
-    # 参数丛书Ji经网络不包括softmax层的前向传播结果，第二个是训练数据的正确答案。因为
-    # 标准干年来是a个氏度为10的一维数组，而该函数需要提供的是一个正确答案的数字，所以需要
-    # 使用tf.argmax函数来得到正确答案对应的类别编号。
+    # 计算交叉熵作为刻画预测值和真实值之间差距的损失函数。使用了TensorFlow中提供的sparse_softmax_cross_entropy_with_logits来计算交叉熵。
+    # 当分类问题只打一个正确答案时，可以使用这个函数来加速交叉熵的计算。MNIST问题的图片中只n包含了
+    # 0～9的－个数字，所以可以使用这个函数来计算交叉熵损失。这个函数的第一个参数是神经网络
+    # 不包括softmax层的前向传播结果，第二个是训练数据的正确答案。因为标准答案是一个长度为10的一维数组，
+    # 而该函数需要提供的是一个正确答案的数字，所以需要使用tf.argmax函数来得到正确答案对应的类别编号。
     cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y, 1))
-    # 计算在、＂1前batch中所有样例的交叉娟平均值。
+    # 计算在当前batch中所有样例的交叉熵平均值。
     cross_entropy_mean = tf.reduce_mean(cross_entropy)
     # 计算12正则化损失函数。
     regularizer = tf.contrib.layers.l2_regularizer(REGULARIZATION_RATE)
     # 一般只计算神经网络边上权重的正则化损失，而不使用伺。
     regularization = regularizer(weights1) + regularizer(weights2)
-    # 总损失悖于交叉悄损失利正则化损失的利。
+    # 总损失等于交叉熵损失和正则化损失的利。
     loss = cross_entropy_mean + regularization
-    # 设咒指数衰减的学习率。
+    # 设置指数衰减的学习率。
 
     learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE, global_step, mnist.train.num_examples / BATCH_SIZE,
                                                LEARNING_RATE_DECAY)
-    # 基础的学习率，随着法代的进行，旦新变盐｜付使用的
-    # 学习率在这个基础上递减。
-    # 当前途代的轮数。
-    # 过完所有的训练数据南耍的法# 代次数。
-    # 学习率衰减速度。
+    # LEARNING_RATE_BASE基础的学习率，随着迭代的进行，更新变量时使用的学习率在这个基础上递减。
+    # global_step当前途代的轮数。
+    # mnist.train.num_examples / BATCH_SIZE过完所有的训练数据需要的迭代次数。
+    # LEARNING_RATE_DECAY学习率衰减速度。
     # 使用tf.train.GradientDescentOptimizer优化算法来优化损失函数。注意这里损失函数
 
-    # 包含了交叉；脑损失和12正则化损失。
+    # 包含了交叉熵损失和12正则化损失。
     train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
     # 在训练、神经网络模型时，每过一遍数据既需要通过反向传播来更新神经网络中的参数，
-    # 又要更新每一个参数的滑到J平均值。为了一次完成多个操作，TensorFlow提供了
+    # 又要更新每一个参数的滑到平均值。为了一次完成多个操作，TensorFlow提供了
     # tf.controldependencies和tf.group两种机制。下面两行程序和l
     # train_op=tf.group(train_step,variables_averages_op）是等价的。
     with tf.control_dependencies([train_step, variables_averages_op]):
         train_op = tf.no_op(name='train')
     # 检验使用了滑动平均模型的神经网络前向传播结果是否正确。tf.argmax(average_y,1)
-    # 计算每一个样例的预测答案。其中average_y是一个batch_size食10的二维数组，每一行
-    # 表示一个样例的前向传播结果。tf.argmax的第二个参数＂l”表示选取最大值的操作仅在第一
+    # 计算每一个样例的预测答案。其中average_y是一个batch_size 10的二维数组，每一行
+    # 表示一个样例的前向传播结果。tf.argmax的第二个参数＂1”表示选取最大值的操作仅在第一
     # 个维度中进行，也就是说，只在每一行选取最大值对应的下标。于是得到的结果是一个长度为
     # batch的一维数组，这个一维数组中的值就表示了每一个样例对应的数字识别结果。tf.equal
     # 判断两个张量的每一维是否相等，如果相等返回True，否则返回False。
--- tensorflow/chapter5/note.md
+++ tensorflow/chapter5/note.md
@@ -3,4 +3,9 @@
 ## 5.1 MNIST 数据处理
 
 input_data.read_ data_ sets 函数生成的类会自动将 MNIST 数据集划分为 train、 validation 和 test 三个数据集
+
+## 5.2 神经网络模型训练及不同 模型结果对比
+
+    5.2.py
+
 ### 5.2.2 使用验证数据集判断模型效果
