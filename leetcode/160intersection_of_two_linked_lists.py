45fe5589bfb56fd6c5d1d10ff5acfa54c6043c08
Jane <shaowenqin620@163.com>
2019-10-17 23:06:32 +0800

    160

--- leetcode/160intersection_of_two_linked_lists.py
+++ leetcode/160intersection_of_two_linked_lists.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution(object):
     def getIntersectionNode(self, headA, headB):
@@ -10,23 +10,21 @@ class Solution(object):
         :type head1, head1: ListNode
         :rtype: ListNode
         """
-        curA = headA
-        curB = headB
-        l1 = []
-        while curA:
-            l1.append(curA.val)
-            curA = curA.next
-        while curB:
-            t = curB
-            if t:
-                l2 = l1[l1.index(t.val):]
-            index = 0
-            while t and l2:
-                if t.val == l2[index]:
-                    t = t.next
-                    index += 2
-                else:
-                    l2 = l2[l1.index(curB.val)+1:]
-                    break
-            if not t and not l2.[index]:return True
-            elif t
+        pa = headA
+        pb = headB
+
+        while pa is not pb:
+            # if either pointer hits the end, switch head and continue the second traversal,
+            # if not hit the end, just move on to next
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next
+
+        return pa # only 2 ways to get out of the loop, they meet or the both hit the end=None
+
+
+# Approach 3: Two Pointers
+# Maintain two pointers pApA and pBpB initialized at the head of A and B, respectively. Then let them both traverse through the lists, one node at a time.
+# When pApA reaches the end of a list, then redirect it to the head of B (yes, B, that's right.); similarly when pBpB reaches the end of a list, redirect it the head of A.
+# If at any point pApA meets pBpB, then pApA/pBpB is the intersection node.
+
+# If two lists have intersection, then their last nodes must be the same one. So when pApA/pBpB reaches the end of a list, record the last element of A/B respectively. If the two last elements are not the same one, then the two lists have no intersections.
