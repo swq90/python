d92d42e2308266eab936e8836caba3829d36ecc8
Jane <shaowenqin620@163.com>
2020-05-08 23:19:15 +0800

    c

---
+++ leetcode/993Cousins_in_Binary_Tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        t=root
+
+        def position(root,x):
+            parent=root
+            if parent.val==x:
+                return False
+            els
+        return
