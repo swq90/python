79c940d9f3c9a15468fa014481f655555f3578ae
Jane <shaowenqin620@163.com>
2020-08-04 22:12:56 +0800

    del

--- numpy/basics.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from numpy import *
-b = np.floor(10*np.random.random((1,5)))
-
-a = np.floor(10*np.random.random(1,5))
-print(b,a)
-print(column_stack((a,b)))
-
-c.flags.owndata
