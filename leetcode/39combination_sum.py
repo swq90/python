4b68e8abdcd25c87cef814bbec1a3082f1c2ab90
Jane <shaowenqin620@163.com>
2020-10-02 21:18:13 +0800

    d

--- leetcode/39combination_sum.py
+++ leetcode/39combination_sum.py
@@ -1,11 +1,3 @@
 class Solution:
     def combinationSum(self, candidates, target: int) :
-        l = []
-        if sum == target:
-            l.append()
-        elif sum > target:
-            return#返回上上层
-        else:
-            #继续下去
-            pass
-        pre =[p+c for p in pre for c in candidates]
+        def add_s()
