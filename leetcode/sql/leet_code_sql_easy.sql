256f811c547561959caa377da23d1b47251105ba
Jane <shaowenqin620@163.com>
2020-10-31 21:50:04 +0800

    leetcode sql

--- leetcode/sql/leet_code_sql_easy.sql
+++ leetcode/sql/leet_code_sql_easy.sql
@@ -1,4 +1,5 @@
 -- Easy
+
 -- 175. conbine two tables
 select FirstName, LastName, City, State
 from Person a
@@ -8,4 +9,16 @@ from Person a
 select max(salary) as SecondHighestSalary
 from employee
 where Salary < (select max(salary) from Employee);
+
 -- 182. Duplicate Emails
+select Email from Person group by Email having count(Email)>1;
+
+-- 183. Customers Who Never Order
+select Name as Customers from (Customers a left join Orders b on a.Id=b.CustomerId ) where Customerid is null;
+select name as customers from customers where customers.id not in (select customerid from orders);
+
+-- 196. Delete Duplicate Emails
+
+
+-- 596. Classes More Than 5 Students
+select class as class from courses group by class having  count(distinct student)>4;
